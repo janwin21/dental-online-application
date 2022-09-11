f93f16c2456a0f206d0ffff284de269e5d4f2fc5
Eduardo Toralba <edtoralba.jr@gmail.com>
2022-09-12 03:38:18 +0800

    mouth masters v.1.4

---
+++ layout/resources/js/model/intraoral-selection.js
@@ -0,0 +1,45 @@
+// INTRAORAL SELECTION SCRIPT
+'use strict';
+
+function IntraoralSelection(parent, settings) {
+    this.parent = parent;
+    this.settings = settings;
+
+    this.template = `<!-- Intraoral Selection Template -->
+        <div class="col-3 p-1">
+            <button type="button" class="intraoral-btn btn btn-light m-0 p-0 shadow-none w-100" data-acronym="{{ acronym }}" data-name="{{ name }}">
+                <div class="text-center rounded px-2 pb-1 mt-0 shadow">
+                    <h3 class="anton mx-0 py-2 p-0">{{ acronym }}</h3>
+                    <p class="intraoral-name roboto m-0 mb-1 p-0 fs-xs">{{ name }}</p>
+                </div>
+            </button>
+        </div>
+    `;
+
+    this.setDetails = () => {
+        let { conditions, restorations, surgeries } = this.settings;
+        this.setDetail(conditions, 0);
+        this.setDetail(restorations, 1);
+        this.setDetail(surgeries, 2);
+
+        return $('.intraoral-btn');
+    };
+
+    this.setDetail = (elements, index) => {
+        let templateText = '';
+
+        elements.forEach(element => {
+            let { acronym, name } = element;
+            templateText += this.template
+                .replaceAll('{{ acronym }}', acronym)
+                .replaceAll('{{ name }}', name);
+        });
+
+        this.parent.eq(index).html(templateText);
+    };
+
+    this.setInput = input => { this.input = input; }
+    this.setValue = value => { 
+        if(this.input) this.input.val(value); 
+    }
+};
---
+++ layout/resources/js/pages/intraoral-form.js
@@ -0,0 +1,36 @@
+// INTRAORAL FORM SCRIPT
+'use strict';
+
+$(document).ready(() => {
+
+    const intraoralAcronym = $('.intraoral-acronym');
+    const intraoralName = $('.intraoral-name');
+
+    new IntraoralTable($('.intraoral table'), intraoralSettings).setDetails();
+    
+    const intraoralSelection = new IntraoralSelection($('.navigation-section .row'), legendSettings);
+    const intraoralBtns = intraoralSelection.setDetails();
+
+    intraoralBtns.on('mouseover', event => {
+        let { acronym, name } = event.currentTarget.dataset;
+        intraoralAcronym.text(acronym);
+        intraoralName.text(name);
+    });
+
+    intraoralBtns.on('click', event => {
+        let { acronym } = event.currentTarget.dataset;
+        intraoralSelection.setValue(acronym);
+    });
+
+    let intraoralInputs = $('.intraoral-input');
+
+    intraoralInputs.on('click', event => {
+        let input = $(event.currentTarget);
+
+        intraoralInputs.addClass('border-secondary border-1').removeClass('border-success border-2');
+        input.addClass('border-success border-2');
+
+        intraoralSelection.setInput(input);
+    });
+
+});
